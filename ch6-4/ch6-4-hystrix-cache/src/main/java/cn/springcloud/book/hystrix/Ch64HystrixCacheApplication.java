aa03575ad96bd30fc9ac5a1bc2613e8c5ca51e23
mini-break <mini_break@163.com>
2020-02-24 22:50:10 +0800

    增加注示

--- ch6-4/ch6-4-hystrix-cache/src/main/java/cn/springcloud/book/hystrix/Ch64HystrixCacheApplication.java
+++ ch6-4/ch6-4-hystrix-cache/src/main/java/cn/springcloud/book/hystrix/Ch64HystrixCacheApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -14,7 +13,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableHystrix
-public class HystrixCacheApplication {
+public class Ch64HystrixCacheApplication {
 
 	@Bean
 	@LoadBalanced
@@ -23,7 +22,7 @@ public class HystrixCacheApplication {
     }
 	
     public static void main(String[] args) {
-        SpringApplication.run(HystrixCacheApplication.class, args);
+        SpringApplication.run(Ch64HystrixCacheApplication.class, args);
     }
    
 }
